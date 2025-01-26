3679def02874508769eac06f23a41ca675773d71
StevenJ94 <stevenruiz.p94@gmail.com>
2025-01-26 17:40:25 -0500

    tabla lista

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
 //@ts-ignore
 import { reports } from '../../../assets/procedimientos'; // Ajusta la ruta según la ubicación de tu archivo
-
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  lista = reports
-  constructor() {
-    console.log(this.lista);
+  /**
+   * Lista en donde vienen los reporte
+   */
 
+
+  lista = reports
+
+
+  constructor() {
+    this.lista.forEach((item: any) => {
+      item.seleccionado = false
+    });
+    console.log(this.lista);
+  }
+
+
+
+  reporte(item: any, event: MouseEvent): void {
+    event.stopPropagation();
+    console.log('Generando reporte para:', item);
+  }
+
+  visualizar(item: any, event: MouseEvent): void {
+    event.stopPropagation();
+    console.log('Visualizando:', item);
   }
 }
